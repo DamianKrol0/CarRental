3dae787e1e6e813b06581d656b91fb22057a07d7
krol.d <krol.d@e80group.com>
2024-01-18 15:10:32 +0100

    poprawienie widoku wyświetlania

---
+++ CarRental.MVC/Areas/Identity/Pages/_ViewStart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CarRental.MVC.Areas.Identity.Page
+{
+    public class _ViewStartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
